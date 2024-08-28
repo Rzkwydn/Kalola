2244c0daf1d8fe384fce1f07bb699c7e2a024e43
Rzkwydn <rzkwydn@gmail.com>
2024-08-28 22:30:27 +0700

    Noting

--- kalola-mobile/src/navigation/DrawerNavigator.tsx
+++ kalola-mobile/src/navigation/DrawerNavigator.tsx
@@ -2,36 +2,32 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import DashboardScreen from '../screens/DashboardScreen';
-import SalesManagementScreen from '../screens/Sales/SalesManagementScreen';
 import InventoryScreen from '../screens/Inventory/InventoryScreen';
 import SalesScreen from '../screens/Sales/SalesScreen';
 import TransactionScreen from '../screens/Transaction/TransactionScreen';
 import SettingScreen from '../screens/Setting/SettingScreen';
-import StackTransaction from './StackNavigator/StackTransaction';
-import DrawerNavigator from './DrawerNavigator';
 
-export type MainStackParamList = {
-  DrawerNav: undefined;
+export type DrawerParamList = {
+  Dashboard: undefined;
   StackTransaction: undefined;
-  Transaction: undefined;
   Sales: undefined;
   Inventory: undefined;
   Setting: undefined;
 };
 
-const Stack = createDrawerNavigator<MainStackParamList>();
+const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
-const MainNavigator: React.FC = () => {
+const DrawerNavigator: React.FC = () => {
   return (
-    <Stack.Navigator screenOptions={{ headerShown:true}}>
+    <Drawer.Navigator initialRouteName="Dashboard">
+      <Stack.Screen name="Dashboard" component={DashboardScreen} />
+      <Stack.Screen name='StackTransaction' component={TransactionScreen} />
       <Stack.Screen name="Sales" component={SalesScreen} />
       <Stack.Screen name="Inventory" component={InventoryScreen} />
       <Stack.Screen name='Setting' component={SettingScreen} />
-      <Stack.Screen name="DrawerNav" component={DashboardScreen}/>
-      <Stack.Screen name='Transaction' component={TransactionScreen} />
-      <Stack.Screen name='StackTransaction' component={StackTransaction} options={{ headerShown : false}} />
-    </Stack.Navigator>
+    </Drawer.Navigator>
   );
 };
 
-export default MainNavigator;
+export default DrawerNavigator;
--- kalola-mobile/src/screens/Transaction/TransactionScreen.tsx
+++ kalola-mobile/src/screens/Transaction/TransactionScreen.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { globalStyles } from '../../styles/globalStyles';
+import { StackTransactionParamList } from '../../navigation/StackNavigator/StackTransaction';
+import { StackNavigationProp } from '@react-navigation/stack';
 
-const TransactionScreen: React.FC = () => {
+type TransactionNavigationProp = StackNavigationProp<StackTransactionParamList, 'Transaction'>;
+
+type Props = {
+  navigation: TransactionNavigationProp;
+};
+
+const TransactionScreen: React.FC < Props >= ({navigation}) => {
   return (
     <View style={globalStyles.container}>
       <Text style={globalStyles.title}>Transaction</Text>
       {/* Implement dashboard content here */}
+      <Text
+          style={globalStyles.linkText}
+          onPress={() => navigation.navigate('CartItem')}
+        >
+          Cart
+        </Text>
     </View>
   );
 };
